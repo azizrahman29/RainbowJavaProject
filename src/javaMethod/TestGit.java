9f73a4f8c90d02fa4759380b3dd335798e296ad4
azizrahman29 <128001406+azizrahman29@users.noreply.github.com>
2023-04-01 10:30:22 -0400

    Update TestGit.java
    Follow the comment in line 5

--- src/javaMethod/TestGit.java
+++ src/javaMethod/TestGit.java
@@ -2,6 +2,7 @@ package javaMethod;
 
 public class TestGit {
 	public static void main(String[] args) {
+		// don't call this method in a static way, go for an object of the class
 
 		display();
 	}
