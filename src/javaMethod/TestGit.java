d77aa6de70afedd24e55c7a6b383a9208a1b2d54
azizrahman29 <azizrahman2029@gmail.com>
2023-04-01 10:23:33 -0400

    created TestGit.java method to loop from 10 - 0

---
+++ src/javaMethod/TestGit.java
@@ -0,0 +1,15 @@
+package javaMethod;
+
+public class TestGit {
+	public static void main(String[] args) {
+
+		display();
+	}
+
+	public static void display() {
+		for (int i = 10; i > +0; i--) {
+			System.out.println("i: " + i);
+		}
+	}
+
+}
